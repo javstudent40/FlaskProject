530ec5eb36cc3b33a7f9b0f58a6d4d5618d72e7c
javstudent40 <titusgr30@yahoo.com>
2021-02-20 18:09:15 -0500

    fixed datetime error

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from __init__ import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    data = db.Column(db.String(10000))    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
